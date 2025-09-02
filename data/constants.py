0a48f2a059ad6928a5345c9c02d4ed4827e77a08
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-09-02 19:30:24 -0400

    first commit

---
+++ data/constants.py
@@ -0,0 +1,10 @@
+# Mass of planet in kilograms
+planetary_mass = 5.972e24
+# Radius of the planet in kilometers
+planetary_radius = 6378
+# Newtons Gravitational Constant
+gravitational_constant = 6.67e-11
+# Distance from Planet in kilometers
+distance = 500
+# Corrected Radius
+corrected_rad = planetary_radius + distance
