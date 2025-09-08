6c6612935dbc8f866dece86a026330df68d1b908
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-09-07 20:23:59 -0400

    Added a slider to control the speed of animation but it's very choppy when moving fast with jumps

--- visualization/vpython_scene.py
+++ visualization/vpython_scene.py
@@ -1,6 +1,6 @@
 import numpy as np
 from fontTools.subset import retain_empty_scripts
-from vpython import sphere, vec, rate, color, scene, textures, checkbox, wtext
+from vpython import sphere, vec, rate, color, scene, textures, checkbox, wtext, slider
 from physics.motion import acceleration
 from data.constants import v0, i
 import datetime
@@ -27,9 +27,22 @@ time_sim = 0.0
 rotation_x_matrix = np.array([[1,0,0],[0,np.cos(i), -np.sin(i)], [0,np.sin(i), np.cos(i)]])
 wtext(text="\n\n")
 time_display = wtext(text="Elapsed time: 0s")
+wtext(text="\n\n")
+dt_scalar = 1
+min_value = 0.25
+max_value = 10
+def scale(evt):
+    global dt_scalar
+    dt_scalar = min_value + (max_value - min_value) * evt.value
+    dt_slider_text.text = "{:1.2f}".format(dt_scalar)
+
+dt_scalar_slider = slider(bind=scale, minval=0, maxval=1, value=0.01)
+
+dt_slider_text = wtext(text="{:1.2f}".format(dt_scalar_slider.value * 10))
+
 
 def animate_orbit_3d(r0, T, dt):
-    global time_sim, time_display
+    global time_sim, time_display, dt_scalar
     ball = sphere(
     pos=vec(*r0) / SCALE, radius=0.3, color=color.white, make_trail=True, trail_radius=0.02, retain=50)
     earth = sphere(pos=vec(0, 0, 0), radius=6378e3 / SCALE, texture=textures.earth)
@@ -40,11 +53,11 @@ def animate_orbit_3d(r0, T, dt):
     v = np.array([0, 0, v0]) @ rotation_x_matrix
     while True:
         rate(60)
-
+        dt_eff = dt * dt_scalar
         #Update the total elapsed time in seconds
-        time_sim += float(dt)
+        time_sim += float(dt_eff)
         #Calculate earths rotation based on elapsed time in seconds
-        earth.rotate(angle=2*np.pi/86400 * dt, axis=vec(0, 1, 0))
+        earth.rotate(angle=2*np.pi/86400 * dt_eff, axis=vec(0, 1, 0))
         #Update the elapsed time widget text
         time_display.text = f"Elapsed time: {str(datetime.timedelta(seconds=time_sim))}"
         if follow_satellite:
@@ -55,12 +68,12 @@ def animate_orbit_3d(r0, T, dt):
 
         a = acceleration(r)  # old acceleration
         r_new = (
-            r + v * dt + 0.5 * a * dt**2
+            r + v * dt_eff + 0.5 * a * dt_eff**2
         )  # update the new position with the guess from the old acceleration
         a_new = acceleration(
             r_new
         )  # get new acceleration with the new guessed position
-        v_new = v + 0.5 * (a + a_new) * dt  # calculate new velocity
+        v_new = v + 0.5 * (a + a_new) * dt_eff  # calculate new velocity
         # Update the radius and velocity variables
         r = r_new
         v = v_new
